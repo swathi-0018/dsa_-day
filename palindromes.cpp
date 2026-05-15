a233de5d9d80ea62f7e136efe14124d687ec7bf8
Swathi-0018 <swativb009@gmail.com>
2026-05-15 18:39:48 +0530

    palindromes

---
+++ palindromes.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+ int main(){
+    int rever= 0;
+    int ld;
+    int n;
+    cin>> n;
+    int dup = n;
+    while (n>0){
+        ld=n%10;
+        n=n/10;
+        rever= (rever*10)+ld;
+
+    }
+    if(dup==rever){
+        cout <<"palidrome";
+    }
+    else{
+        cout <<"not a palidrome";   
+    }
+        return 0;
+ }
