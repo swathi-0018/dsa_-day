5504e4b6367f9c9a07ce56f21d17e775d4fd9e47
Swathi-0018 <swativb009@gmail.com>
2026-05-16 12:16:13 +0530

    seclarge.cpp

---
+++ seclarge.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int sec_largest(int *n, int *arr){
+    int larg=arr[0];
+    int slargest = -1;
+    for (int i=0;i<*n;i++){
+        if(arr[i]>larg){
+            slargest=larg;
+            larg =arr[i];
+        }
+        if(arr[i]>slargest&&arr[i]<larg){
+            slargest =arr[i];
+
+        }
+
+    }
+    return slargest;
+
+}
+int main(){
+    int n;
+    cin>> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>> arr[i];
+
+    }
+    int sl=sec_largest(&n,arr);
+    if(sl==-1){
+        cout <<"no second largest element";
+    }
+    else{
+        cout <<"second largest element is " << sl;
+    }   
+    return 0;
+}
