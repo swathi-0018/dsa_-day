6e394b6d4fb04c2cc4a1286fa760c6db34cf2231
Swathi-0018 <swativb009@gmail.com>
2026-05-16 13:06:01 +0530

    secsmall

---
+++ secsmall.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int sec_smallest(int n, int arr[]) {
+
+    int small = arr[0];
+    int ssmall = INT_MAX;
+
+    for (int i = 1; i < n; i++) {
+
+        if (arr[i] < small) {
+            ssmall = small;
+            small = arr[i];
+        }
+
+        else if (arr[i] != small && arr[i] < ssmall) {
+            ssmall = arr[i];
+        }
+    }
+
+    return ssmall;
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int ssmall = sec_smallest(n, arr);
+
+    cout << ssmall;
+
+    return 0;
+}
