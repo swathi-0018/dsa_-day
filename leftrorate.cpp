6c44f07d6bf28d569c4ae12d7fb6c8a669a25637
Swathi-0018 <swativb009@gmail.com>
2026-05-17 13:21:06 +0530

    leftrotate

---
+++ leftrorate.cpp
@@ -0,0 +1,35 @@
+// left rotation of an array by d places
+#include <iostream>
+using namespace std;
+
+void left_rotate (int arr[],int n)
+{
+    int d= d%n;
+    int temp[d];
+    for (int i=0;i<d;i++){
+        temp[i]=arr[i];
+    }
+    for (int i=d;i<n;i++){
+        arr[i-d] = arr[i];
+    }
+    for(int i= n-d;i<n;i++){
+        arr[i] = temp[i-(n-d)];
+    }
+    return;
+    }
+
+    int main(){
+        int n ;
+        int d;
+        cin >> n >> d;
+        int arr[n];
+        for (int i=0;i<n;i++){
+            cin >> arr[i];
+        }
+        left_rotate(arr,n);
+        cout << "Array after left rotation: ";
+        for (int i=0;i<n;i++){
+            cout << arr[i] << " ";
+        }
+        return 0;
+    }
