cee77c82649470ca7235b9642066c328467877e9
Swathi-0018 <swativb009@gmail.com>
2026-05-19 19:14:51 +0530

    intersection

--- intersection.cpp
+++ intersection.cpp
@@ -3,33 +3,30 @@
 
 using namespace std;
 
-vector<int> sortedArray(vector<int> a, vector<int> b){
-
-    int n1 = a.size();
-    int n2 = b.size();
-
-    int i = 0;
-    int j = 0;
-
+vector <int> sortedArray(vector<int> a,vector<int> b){
     vector<int> ans;
+
+    int i=0;
+    int j= 0;
+    int n1;
+    int n2;
+
     while (i<n1 && j<n2){
-        if(a[i] < b[j]){
+        if(a[i]<b[j]){
             i++;
         }
-        else {
-            if(b[j]<a[i]){
-                j++;
-            }
-            else {
-                ans.push_back(a[i]);
-                i++;    
-                j++;
-
-            }
-            }
+        else if(b[j]<a[i]){
+            j++;
         }
-    return ans;
+        else 
+        {
+            ans.push_back(a[i]);
+            i++;
+            j++;
     }
+}    
+    return ans;
+}
     int main(){
 
     vector<int> a = {1,2,2,3,4,5};
@@ -38,7 +35,7 @@ vector<int> sortedArray(vector<int> a, vector<int> b){
     vector<int> ans = sortedArray(a,b);
 
     for(auto x : ans){
-        cout << x << " ";
+        cout << x << " ";  
     }
 
     return 0;
