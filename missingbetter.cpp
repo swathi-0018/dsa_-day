4b29d44475d4332bf16af95c0fa35c27fc2e381e
Swathi-0018 <swativb009@gmail.com>
2026-05-20 22:01:19 +0530

    missingbetter.cpp

---
+++ missingbetter.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void missing(int arr[],int n){
+    int hash[n+1] ={0};
+    for (int i = 0; i<n;i++){
+        hash[arr[i]]=1;
+    }
+    for(int i=1;i<=n+1;i++){
+        if(hash[i] == 0){
+            cout << i;
+            break;
+        }
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    missing(arr, n);
+    
+    return 0;
+}
