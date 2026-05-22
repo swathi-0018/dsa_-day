da59a541431fc30678d1fbf2c031d88643b4cc04
Swathi-0018 <swativb009@gmail.com>
2026-05-22 18:47:33 +0530

    euclidean.cpp

---
+++ euclidean.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin >> a >> b;
+
+    while(a>0 && b>0){
+        if ( a > b){
+            a= a%b;
+        }
+        else{
+            b=b%a;
+        }
+        if (a==0){
+            cout << b;
+        }
+        else if (b==0){
+            cout << a;
+        }
+        }
+        return 0;
+    }
