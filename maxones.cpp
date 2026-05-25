23876bdc16ac7b62209032b23fd3682c6fc0d230
Swathi-0018 <swativb009@gmail.com>
2026-05-25 20:11:43 +0530

    maxones.cpp

---
+++ maxones.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+void maxoness(int arr[],int n){
+   int count =0;
+   int maxi =0;
+   for (int i=0;i<n;i++){
+    if(arr[i]==1){
+        count++;
+        if(count>maxi){
+            maxi=count;
+
+        }
+        else{
+            count=0;
+        }
+    }
+    
+   }
+   cout << maxi;
+}
+int main(){
+    int n;
+    cin>> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>> arr[i];
+    }
+    maxoness(arr,n);
+  
+
+    return 0;
+    }
+    
