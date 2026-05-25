e4e56ed4dca3cd36f7c991cfc65a67680b6be1df
Swathi-0018 <swativb009@gmail.com>
2026-05-25 20:24:53 +0530

    number_appears_ones

---
+++ number_appears_ones/brute.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int brute(int arr[], int n){
+
+    for(int i = 0; i < n; i++){
+
+        int num = arr[i];
+        int count = 0;
+
+        for(int j = 0; j < n; j++){
+
+            if(arr[j] == num){
+                count++;
+            }
+        }
+
+        if(count == 1){
+            return num;
+        }
+    }
+
+    return -1;
+}
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    cout << brute(arr, n);
+
+    return 0;
+}
