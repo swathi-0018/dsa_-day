0bcdee95f8b6e4a26ef8420449422f36d29af8b0
Swathi-0018 <swativb009@gmail.com>
2026-05-25 20:52:25 +0530

    optimal.cpp

---
+++ number_appears_ones/optimal.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int xr = 0;
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0 ; i<n ; i++){
+        cin >> arr[i];
+    }
+    for(int i = 0 ; i < n ; i++){
+        xr = xr^arr[i];
+
+    }
+    cout << xr;
+    return 0;
+}
