cbda3a85ccb6e9d79e72054d826a02b66c64b7e7
Swathi-0018 <swativb009@gmail.com>
2026-05-30 20:12:53 +0530

    bruteForce.cpp

---
+++ longestSubarrayWithSumK/bruteForce.cpp
@@ -0,0 +1,36 @@
+//longest subarray with given sum k;
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, K;
+    cin >> n;
+
+    int a[n];
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    cin >> K;
+
+    int len = 0;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = i; j < n; j++) {
+
+            int s = 0;
+
+            for(int k = i; k <= j; k++) {
+                s += a[k];
+            }
+
+            if(s == K) {
+                len = max(len, j - i + 1);
+            }
+        }
+    }
+    cout << len;
+
+    return 0;
+}
