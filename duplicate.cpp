86efdc0b2d623bf53e19e47ccebda2889013de9f
Swathi-0018 <swativb009@gmail.com>
2026-06-15 20:00:36 +0530

    duplicate.cpp

--- duplicate.cpp
+++ duplicate.cpp
@@ -1,21 +1,25 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     int n;
     cin >> n;
-    int arr[n];
-    for (int i=0;i<n;i++){
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    int i=0;
-    for(int j=1;j<n;j++){
-        if(arr[j]!=arr[i]){
-            arr[i+1]= arr[j];
+
+    int i = 0;
+    for (int j = 1; j < n; j++) {
+        if (arr[j] != arr[i]) {
+            arr[i + 1] = arr[j];
             i++;
         }
     }
-     int newSize = i + 1;
+
+    int newSize = i + 1;
 
     cout << "Array after removing duplicates: ";
     for (int k = 0; k < newSize; k++) {
@@ -24,4 +28,3 @@ int main(){
 
     return 0;
 }
-
