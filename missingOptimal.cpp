78d7d1b74418609151210aa2570f17626a8cebca
Swathi-0018 <swativb009@gmail.com>
2026-06-20 18:51:41 +0530

    missingOptimal.cpp

--- missingOptimal.cpp
+++ missingOptimal.cpp
@@ -1,7 +1,25 @@
-#include<bits10_1.h>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    
+
+int missingNum(vector<int> &arr) {
+    int n = arr.size() + 1;
+  
+    // Calculate the sum of array elements
+    int sum = 0;
+    for (int i = 0; i < n - 1; i++) {
+        sum += arr[i];
+    }
+
+    // Calculate the expected sum
+    long long expSum = (n *1LL* (n + 1)) / 2;  
+
+    // Return the missing number
+    return expSum - sum;
+}
+
+int main() {
+    vector<int> arr = {8, 2, 4, 5, 3, 7, 1};  
+    cout << missingNum(arr);  
+    return 0;
 }
