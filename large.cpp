497812dddf1be053c8851b1e7dab10c8cfc85229
Swathi-0018 <swativb009@gmail.com>
2026-07-20 19:38:03 +0530

    large.cpp

--- large.cpp
+++ large.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    int arr[n];
+    int arrr[n];
     for (int i= 0;i<n;i++){
-        cin >> arr[i];
+        cin >> arrr[i];
     }
-    int largest = arr[0];
+    int largest = arrr[0];
     for (int i=0;i<n;i++){
-        if(arr[i]>largest){
-            largest = arr[i];
+        if(arrr[i]>largest){
+            largest = arrr[i];
         }
     }
     cout << "largest element is " << largest;
