fa8ab0442b1e8126aeda2d939733ea1e0acf2457
Swathi-0018 <swativb009@gmail.com>
2026-07-26 11:52:24 +0530

    palindrome.cpp

--- palindromes.cpp
+++ palindromes.cpp
@@ -2,14 +2,14 @@
 using namespace std;
  int main(){
     int rever= 0;
-    int ld;
+    int lad;
     int n;
     cin>> n;
     int dup = n;
     while (n>0){
-        ld=n%10;
+        lad=n%10;
         n=n/10;
-        rever= (rever*10)+ld;
+        rever= (rever*10)+lad;
 
     }
     if(dup==rever){
