a05d00390f8b437a06b80515b954a8639ccbbcf6
Swathi-0018 <swativb009@gmail.com>
2026-08-08 21:11:18 +0530

    palindromes.cpp

--- palindromes.cpp
+++ palindromes.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
  int main(){
-    int reverse= 0;
+    int reversee= 0;
     int lad;
     int n;
     cin>> n;
@@ -9,10 +9,10 @@ using namespace std;
     while (n>0){
         lad=n%10;
         n=n/10;
-        reverse= (reverse*10)+lad;
+        reversee= (reversee*10)+lad;
 
     }
-    if(dup==reverse){
+    if(dup==reversee){
         cout <<"palidrome";
     }
     else{
